0035937313abc24e7dd274799629d7bacfc7f2bd
Tushar Goyal <tushargoyal19tj@gmail.com>
2023-09-24 20:07:33 +0530

    5 Select_with_Select.sql

--- Select_with_Select.sql
+++ Select_with_Select.sql
@@ -3,3 +3,19 @@
 where population > ( select population from world
 where name='United Kingdom' ) and population < ( select population from world
 where name='Germany' );
+
+
+/*
+#5 
+Germany (population 80 million) has the largest population of the countries in Europe. 
+Austria (population 8.5 million) has 11% of the population of Germany.
+*/
+select name, concat( round( (population/84432670)*100),'%' ) as percentage from world
+where continent='Europe' ;
+
+-- alternative ans
+
+
+select name, concat( round( (population/ (select population from world where name='Germany')  )*100),'%' ) as percentage from world
+where
+continent='Europe' ;
